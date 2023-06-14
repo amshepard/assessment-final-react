c2e3799485af35efe0b5159b71746e35983b43fe
amshepard <antoineshepard@pursuit.org>
2023-06-14 15:04:15 -0400

    People.js semi-functional.

--- src/Components/People.js
+++ src/Components/People.js
@@ -1,9 +1,63 @@
 // import React from 'react'
 
-function People() {
-  return (
-    <div>People</div>
-  )
-}
+import { useState, useEffect } from "react"
+import { gettingPeople } from "../fetch.js"
+import Error from "./Error.js"
 
-export default People
+export default function People() {
+    const [searchInput, setSearchInput] = useState("")
+    const [people, setPeople] = useState([])
+    const [person, setPerson] = useState([])
+    const [loadingError, setLoadingError] = useState(false)
+
+    async function settingPeople() {
+        const allPeople = await gettingPeople()
+        setPeople(allPeople)
+        console.log(people)
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        const personArr = people.filter((p) => {
+            return p.name.toLowerCase() === searchInput.toLowerCase()
+        })
+        setPerson(personArr)
+        setSearchInput("")
+        !personArr.length ? setLoadingError(true) : setLoadingError(false)
+        console.log(personArr)
+    }
+
+    useEffect(() => {
+        settingPeople()
+    }, [])
+
+    return (
+        <div className="people">
+            <h2>Search for a Person</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    id="search"
+                    value={searchInput}
+                    onChange={(e) => setSearchInput(e.target.value)}
+                />
+                <button className="submit">submit</button>
+            </form>
+            {
+                loadingError ? (<Error />) : (
+                    person.map((x) => {
+                        return (
+                            <aside>
+                                <h2><span>Name: </span>{x.name}</h2>
+                                <p><span>Age: </span>{x.age}</p>
+                                <p><span>Eye Color: </span>{x.eye_color}</p>
+                                <p><span>Hair Color: </span>{x.hair_color}</p>
+                            </aside>
+                        )
+                    })
+                )
+            }
+        </div>
+    )
+}
--- src/fetch.js
+++ src/fetch.js
@@ -1,2 +1,6 @@
 const BASE = "https://resource-ghibli-api.onrender.com"
 
+export function gettingPeople() {
+    return fetch(`${BASE}/people`).then((res) => res.json())
+}
+
